e0c97bb8fea771c228c50b9c463fb29c351d0a59
tmpITK <torok.mark.patrik@gmail.com>
2023-03-21 16:58:14 +0100

    delete more svelte

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<h1 class="text-3xl font-bold underline">Mark's incredible Nft Marketplace</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.blue.100);
-	}
-</style>
