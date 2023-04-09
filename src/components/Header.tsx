3579766ea049c180e8eef1d54ecbd94474d68d6f
tmpITK <torok.mark.patrik@gmail.com>
2023-04-09 10:45:40 +0200

    Minor header fixes and add minting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,10 @@ export default function Header() {
                         </span>
                     </a>
                     <div className="flex items-center lg:order-2">
-                        <WalletConnector />
+                        <div className="dark:text-white">
+                            <WalletConnector />
+                        </div>
+
                         <button
                             data-collapse-toggle="mobile-menu-2"
                             type="button"
@@ -29,9 +32,9 @@ export default function Header() {
                                 xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                 ></path>
                             </svg>
                             <svg
@@ -41,9 +44,9 @@ export default function Header() {
                                 xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                 ></path>
                             </svg>
                         </button>
@@ -80,7 +83,7 @@ export default function Header() {
                             </li>
                             <li>
                                 <a
-                                    href="#"
+                                    href="/mint"
                                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Mint
--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,3 +1,26 @@
 export default function mint() {
-    return <div>Mint</div>
+    return (
+        <div>
+            <button
+                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                onClick={() => callMint()}
+            >
+                Mint
+            </button>
+        </div>
+    )
+}
+
+async function callMint() {
+    const response = await fetch(
+        "http://localhost:5000/0xC75444ef801b50f5601230db66F784e2078BE7Bb",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            },
+        }
+    )
+    console.debug(response)
+    console.debug(await response.text())
 }
