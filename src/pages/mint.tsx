48198f9e4be8b60d0e06cbcc37b1246c611752ab
tmpITK <torok.mark.patrik@gmail.com>
2023-04-09 12:18:15 +0200

    WIP add gatekeeping

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,26 +1,53 @@
+import contracts from "../../constants/abi/contracts.json"
+import { prepareWriteContract, writeContract } from "@wagmi/core"
+import { useAccount } from "wagmi"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+
 export default function mint() {
+    const { address } = useAccount()
+    const { chainId, account, isWeb3Enabled } = useMoralis()
+    console.debug(chainId, account, isWeb3Enabled)
+    // forget moralis, use hardhat maybe? I do not want another service with APis or whatever just to call a goddamn contract
+    async function requestMint(address: `0x${string}`) {
+        const nftMarketplaceArtifact = contracts["31337"][0]["contracts"]["NftMarketplace"]
+
+        const paymentConfig = {
+            abi: nftMarketplaceArtifact!["abi"],
+            contractAddress: nftMarketplaceArtifact!["address"],
+            functionName: "gatekeep",
+            params: {
+                value: 1,
+            },
+        }
+
+        const { runContractFunction } = useWeb3Contract(paymentConfig)
+        try {
+            runContractFunction()
+            console.debug("Money sent!")
+        } catch (error) {
+            console.error(error)
+        }
+
+        /* const response = await fetch(
+            "http://localhost:5000/0xC75444ef801b50f5601230db66F784e2078BE7Bb",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                },
+            }
+        )
+        console.debug(response)
+        console.debug(await response.text()) */
+    }
     return (
         <div>
             <button
                 className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                onClick={() => requestMint()}
+                onClick={() => requestMint(address!)}
             >
                 Mint
             </button>
         </div>
     )
-}
-
-async function requestMint() {
-    const response = await fetch(
-        "http://localhost:5000/0xC75444ef801b50f5601230db66F784e2078BE7Bb",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            },
-        }
-    )
-    console.debug(response)
-    console.debug(await response.text())
 }
