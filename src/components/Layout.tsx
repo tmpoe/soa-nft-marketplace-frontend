52495bc66dae3ebe2117318d2446542456b2adb0
tmpITK <torok.mark.patrik@gmail.com>
2023-04-10 10:53:46 +0200

    Query the graph

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,21 @@
 import Header from "./Header"
-import React from "react"
-import { ExampleQueryDocument, ExampleQueryQuery, execute } from "graphclient"
+import React, { useEffect } from "react"
+import { GetLatestOwnerNftDocument, GetLatestOwnerNftQuery, execute } from "../../.graphclient"
+import { useAccount } from "wagmi"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const { address } = useAccount()
+
+    const [data, setData] = React.useState<GetLatestOwnerNftQuery>()
+    const [latestTokenid, setLatestTokenid] = React.useState<string>()
+    // TODO make it interval queried
+    useEffect(() => {
+        execute(GetLatestOwnerNftDocument, { owner: address }).then((result) => {
+            setData(result?.data)
+        })
+        console.log("Nfts for", address)
+        console.log(data)
+    }, [setData])
     return (
         <div>
             <Header />
