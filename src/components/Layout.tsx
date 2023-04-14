710e3c46d35325bcf384dad432aa98b996a852a6
tmpITK <torok.mark.patrik@gmail.com>
2023-04-14 17:40:14 +0200

    Add nft minted event listener

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header"
+import web3 from "../../provider/web3"
 import React, { useEffect } from "react"
 import { GetLatestOwnerNftDocument, GetLatestOwnerNftQuery, execute } from "../../.graphclient"
 import { useAccount } from "wagmi"
@@ -11,29 +12,40 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
     async function getLatestNft() {
         const result = await execute(GetLatestOwnerNftDocument, { owner: address })
-        return result?.data.nftMinteds[0].tokenId
+        if (!result) {
+            throw new Error("Failed to get latest NFT")
+        }
+        return result.data.nftMinteds[0].tokenId
     }
 
     async function initLatestNft() {
-        const tokenId = await getLatestNft()
-        console.debug("address", address)
-        console.debug("initLatestNft", tokenId)
-        setLatestTokenid(tokenId)
+        try {
+            const tokenId = await getLatestNft()
+            console.debug("address", address)
+            console.debug("initLatestNft", tokenId)
+            setLatestTokenid(tokenId)
+        } catch (error) {
+            console.debug(error)
+        }
     }
 
     function updateLatestNft() {
         timer =
             !timer &&
             setInterval(async () => {
-                const newTokenId = await getLatestNft()
-                if (newTokenId != latestTokenid) {
-                    console.debug("Latest nft changed!")
-                }
-                setLatestTokenid(newTokenId)
+                try {
+                    const newTokenId = await getLatestNft()
+                    if (newTokenId != latestTokenid) {
+                        console.debug("Latest nft changed!")
+                    }
+                    setLatestTokenid(newTokenId)
 
-                console.debug("address", address)
-                console.debug("latest", latestTokenid)
-                console.debug("new", newTokenId)
+                    console.debug("address", address)
+                    console.debug("latest", latestTokenid)
+                    console.debug("new", newTokenId)
+                } catch (error) {
+                    console.debug(error)
+                }
             }, 1000)
     }
 
