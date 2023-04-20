4c5923e7b11d64a772b27c30fca216ba05487e8e
tmpITK <torok.mark.patrik@gmail.com>
2023-04-20 19:12:05 +0200

    Add ownert nft getter skeleton

--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 interface NFTCardElement {
     owner: string
-    hash: string
+    id: string
     image: string
 }
 
@@ -15,7 +15,7 @@ export default function NftCard({ posts }: { posts: Array<NFTCardElement> }) {
                         <img className="object-cover w-full h-48" src={items.image} alt="image" />
                         <div className="p-4">
                             <h4 className="text-xl font-semibold text-blue-600">{items.owner}</h4>
-                            <p className="mb-2 leading-normal">{items.hash}</p>
+                            <p className="mb-2 leading-normal">{items.id}</p>
                             <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
                                 Read more
                             </button>
--- src/pages/userCollection.tsx
+++ src/pages/userCollection.tsx
@@ -2,51 +2,73 @@ import { useAccount } from "wagmi"
 import { GetOwnerNftsDocument, execute } from "../../.graphclient"
 import { useEffect, useState } from "react"
 import NftCard from "@/components/NftCard"
+import web3 from "../../provider/web3"
+import contracts from "../../constants/abi/contracts.json"
+import { AbiItem } from "web3-utils"
 
 export default function UserCollection() {
     const { address } = useAccount()
+    const [nftData, setNftData] = useState([])
     const [nfts, setNfts] = useState([])
 
-    async function getOwnerNfts() {
+    async function getOwnerNftData() {
         const result = await execute(GetOwnerNftsDocument, { owner: address })
         if (!result) {
             throw new Error("Failed to get NFTs")
         }
-        setNfts(result.data.nftMinteds)
+        setNftData(result.data.nftMinteds)
     }
+
+    async function getOwnerNfts() {
+        const chainId: number = await web3.eth.getChainId()
+        const nftArtifact =
+            contracts[chainId.toString() as keyof typeof contracts][0]["contracts"]["Nft"]
+
+        const nftContract = new web3.eth.Contract(
+            nftArtifact!.abi as AbiItem[], // https://github.com/web3/web3.js/issues/3310
+            nftArtifact!.address
+        )
+        try {
+            // get owner nfts
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     useEffect(() => {
+        getOwnerNftData()
         getOwnerNfts()
     }, [])
-    console.log(nfts)
+    console.log(nftData)
     const n = [
         {
-            owner: nfts[0]["owner"],
-            hash: nfts[0]["tokenId"],
+            owner: nftData[0]["owner"],
+            id: nftData[0]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
         {
-            owner: nfts[1]["owner"],
-            hash: nfts[1]["tokenId"],
+            owner: nftData[1]["owner"],
+            id: nftData[1]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
         {
-            owner: nfts[0]["owner"],
-            hash: nfts[0]["tokenId"],
+            owner: nftData[0]["owner"],
+            id: nftData[0]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
         {
-            owner: nfts[1]["owner"],
-            hash: nfts[1]["tokenId"],
+            owner: nftData[1]["owner"],
+            id: nftData[1]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
         {
-            owner: nfts[0]["owner"],
-            hash: nfts[0]["tokenId"],
+            owner: nftData[0]["owner"],
+            id: nftData[0]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
         {
-            owner: nfts[1]["owner"],
-            hash: nfts[1]["tokenId"],
+            owner: nftData[1]["owner"],
+            id: nftData[1]["tokenId"],
             image: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
         },
     ]
