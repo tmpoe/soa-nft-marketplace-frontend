451f2e38ae7abb1a7c9e1fa08df11bc301900fe4
tmpITK <torok.mark.patrik@gmail.com>
2023-04-22 15:30:25 +0200

    Slice owner string

--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -14,7 +14,9 @@ export default function NftCard({ posts }: { posts: Array<NFTCardElement> }) {
                     <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
                         <img className="object-cover w-full h-48" src={items.image} alt="image" />
                         <div className="p-4">
-                            <h4 className="text-xl font-semibold text-blue-600">{items.owner}</h4>
+                            <h4 className="text-xl font-semibold text-blue-600">
+                                {items.owner.slice(0, 8)}
+                            </h4>
                             <p className="mb-2 leading-normal">{items.id}</p>
                             <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
                                 Read more
