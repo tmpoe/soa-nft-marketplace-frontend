fca5bcd4080cfc8f556c092a7aa7eef5382bc508
tmpITK <torok.mark.patrik@gmail.com>
2023-05-01 16:39:01 +0200

    Revert "Add graph unittesting framework matchstick"
    This reverts commit fa6d57153bbc69bd3f76b5fd0e758c503b7d397e.

--- tests/adapters/thegraph.test.ts
+++
@@ -1,7 +0,0 @@
-import { assert, describe, test } from "matchstick-as/assembly/index"
-
-describe("NftMarketplaceEventDB", () => {
-    test("Should get latest nft for user", () => {
-        assert.assertTrue(true == true)
-    })
-})
