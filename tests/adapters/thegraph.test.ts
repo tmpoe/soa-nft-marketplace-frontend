fa6d57153bbc69bd3f76b5fd0e758c503b7d397e
tmpITK <torok.mark.patrik@gmail.com>
2023-05-01 16:28:27 +0200

    Add graph unittesting framework matchstick

---
+++ tests/adapters/thegraph.test.ts
@@ -0,0 +1,7 @@
+import { assert, describe, test } from "matchstick-as/assembly/index"
+
+describe("NftMarketplaceEventDB", () => {
+    test("Should get latest nft for user", () => {
+        assert.assertTrue(true == true)
+    })
+})
